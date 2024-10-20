e22db00e9a7baf983adf28d694f7d88cc3dd21b1
WpWebr <wpwebr@gmail.com>
2024-10-20 22:44:35 +0300

    правки

--- gulp/setings/setings.mjs
+++ gulp/setings/setings.mjs
@@ -1,9 +1,7 @@
 export const setings = {
-  // dest: 'dist', //название папки с результатами
   ayBuild: false, // ести 'true' то в режиме `development` файлы JS и CSS сжаты и нет `sourcemaps`
-  sprite: 1, // создавать спрайт - команда `gulp svg`
-  // spriteName: 'sprite.svg', // имя файла спрайта
-  spriteDelAtribut: 1, // удаление атрибутов .svg для спрайта (fill,stroke,style)
+  sprite: true, // создавать спрайт - команда `gulp svg`
+  spriteDelAtribut: true, // удаление атрибутов .svg для спрайта (fill,stroke,style)
   avif: false, // создавать .avif
   avifQuality: 90, // качество .avif можно настроить (от 0 до 100). По умолчанию 90
   webp: true, // создавать .webp
@@ -13,6 +11,6 @@ export const setings = {
   noPicture : ['no-picture'],  // если находим этот класс для тега 'img', то не создаем 'picture' (можно ставить несколько классов)
   noPictureDel : false, // удалять классы прописанные в `noPicture`?
   collapseHTML: false, // сжать HTML
-  webpCSS: 1, // поддержка .webp в CSS (если JS добавил слиль 'wemp' к <html> - см. README.md)
-  noCleanCSSfile: 1, // создавать не сжатый файл style.css
+  webpCSS: true, // поддержка .webp в CSS (если JS добавил слиль 'wemp' к <html> - см. README.md)
+  noCleanCSSfile: true, // создавать не сжатый файл style.css
 }
